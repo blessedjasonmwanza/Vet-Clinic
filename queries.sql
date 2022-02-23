aec03f13ce3aef3db132dc0bb3a26b51377a883e
blessedjasonmwanza <mwanzabj@gmail.com>
2022-02-23 13:24:28 +0200

    complete - Query and update animals table

--- queries.sql
+++ queries.sql
@@ -1,4 +1,6 @@
-/*Queries that provide answers to the questions from all projects.*/
+/*Queries that provide answers to the questions from all projects.
+ COMMENTS ARE RESULTS RUN on data inserted form schema only.
+*/
 
 
 SELECT * FROM animals WHERE name LIKE '%mon';
@@ -67,3 +69,169 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 --   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04
 --   4 | Devimon | 2017-05-12    |               5 | t        |        11
 -- (2 rows)
+
+-- Transactions
+BEGIN;
+
+UPDATE animals SET species =' unspecified';
+-- UPDATE 4
+
+
+SELECT * FROM animals;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg |   species    
+-- ----+---------+---------------+-----------------+----------+-----------+--------------
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 |  unspecified
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 |  unspecified
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04 |  unspecified
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 |  unspecified
+-- (4 rows)
+
+ROLLBACK;
+
+SELECT * FROM animals;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | 
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | 
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04 | 
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 | 
+-- (4 rows)
+
+BEGIN;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+SELECT * FROM animals;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04 | 
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 | digimon
+-- (4 rows)
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+SELECT * FROM animals;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04 | 
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 | digimon
+-- (4 rows)
+
+COMMIT;
+
+SELECT * FROM animals;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04 | 
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 | digimon
+-- (4 rows)
+
+BEGIN;
+
+DELETE FROM animals;
+-- DELETE 4
+
+SELECT * FROM animals;
+--  id | name | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+------+---------------+-----------------+----------+-----------+---------
+-- (0 rows)
+
+ROLLBACK;
+
+SELECT * FROM animals;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 | digimon
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04 | pokemon
+-- (4 rows)
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SELECT * FROM animals;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 | digimon
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04 | pokemon
+-- (4 rows)
+
+SAVEPOINT sp1;
+
+UPDATE animals SET weight_kg = (weight_kg * -1);
+
+SELECT * FROM animals;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon  | 2020-02-03    |               0 | t        |    -10.23 | digimon
+--   2 | Gabumon | 2018-11-15    |               2 | t        |        -8 | digimon
+--   4 | Devimon | 2017-05-12    |               5 | t        |       -11 | digimon
+--   3 | Pikachu | 2021-01-07    |               1 | f        |    -15.04 | pokemon
+-- (4 rows)
+
+ROLLBACK TO sp1;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+
+SELECT * FROM animals;
+--  id |  name   | date_of_birth | escape_attempts | neutered | weight_kg | species 
+-- ----+---------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon  | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon | 2018-11-15    |               2 | t        |         8 | digimon
+--   4 | Devimon | 2017-05-12    |               5 | t        |        11 | digimon
+--   3 | Pikachu | 2021-01-07    |               1 | f        |     15.04 | pokemon
+-- (4 rows)
+
+-- How many animals are there
+SELECT COUNT(*) total_animals FROM animals;
+--  total_animals 
+-- ---------------
+--              4
+-- (1 row)
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) as never_escpaed FROM animals WHERE escape_attempts = 0;
+--  never_escpaed 
+-- ---------------
+--              1
+-- (1 row)
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) as average_weight FROM animals;
+--    average_weight    
+-- ---------------------
+--  11.0675000000000000
+-- (1 row)
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT name, MAX(escape_attempts) as max FROM animals GROUP BY neutered, name ORDER BY max DESC LIMIT 1;
+--  Devimon |   5
+-- (1 row)
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MAX(weight_kg) as max_weigth, MIN(weight_kg) AS min_weight FROM animals GROUP by species;
+--  species | max_weigth | min_weight 
+-- ---------+------------+------------
+--  pokemon |      15.04 |      15.04
+--  digimon |         11 |          8
+-- (2 rows)
+
+SELECT species, AVG(escape_attempts) AS avg_escapes  FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
+
+--  avg_escapes 
+-- -------------
+            
+-- (1 row) 
+-- NB commented output sampples changes when data.sql is run
--- schema.sql
+++ schema.sql
@@ -1,19 +1,22 @@
 /* Database schema to keep the structure of entire database. */
 -- \c vet_clinic
 DROP TABLE IF EXISTS animals;
+
 CREATE TABLE animals (
-    id INT PRIMARY KEY NOT NULL,
-    name varchar(100) NOT NULL,
+    id INT  PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(255) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT,
-    neutered BOOLEAN,
+    neutered BOOLEAN,   
     weight_kg DECIMAL
 );
 
-INSERT INTO animals (id, name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES
-(1, 'Agumon','2020-02-03',0,true,'10.23'),
-(2, 'Gabumon','2018-11-15',2,true,'8'),
-(3, 'Pikachu', '2021-01-07',1,false, '15.04'),
-(4, 'Devimon', '2017-05-12', 5, true, '11');
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES
+('Agumon','2020-02-03',0,true,'10.23'),
+('Gabumon','2018-11-15',2,true,'8'),
+('Pikachu', '2021-01-07',1,false, '15.04'),
+('Devimon', '2017-05-12', 5, true, '11');
 
 -- INSERT 0 4
+
+ALTER TABLE animals ADD species varchar(255) NULL;
