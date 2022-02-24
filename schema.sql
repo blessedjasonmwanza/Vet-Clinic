5be34091db0eca8147ca9b0f6863fddebf862d9e
blessedjasonmwanza <mwanzabj@gmail.com>
2022-02-24 13:34:46 +0200

    Add species and owners tables wth reference keys

--- schema.sql
+++ schema.sql
@@ -20,3 +20,26 @@ INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALU
 -- INSERT 0 4
 
 ALTER TABLE animals ADD species varchar(255) NULL;
+
+DROP TABLE IF EXISTS owners;
+
+CREATE TABLE owners(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(255),
+    age INT
+
+);
+
+DROP TABLE IF EXISTS species;
+CREATE TABLE species(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255)
+);
+
+ALTER TABLE animals DROP species;
+
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT species_id FOREIGN KEY (species_id) REFERENCES species (id);
+
+ALTER TABLE animals ADD  owners_id INT;
+ALTER TABLE animals ADD CONSTRAINT owners_id FOREIGN KEY (owners_id) REFERENCES owners(id);
