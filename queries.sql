27e6e0b928f7f05f1f1cc9de01b337d0d92113d1
Natalia Poletaeva <natfly799@gmail.com>
2022-03-08 13:34:18 +0500

    Add queries for checking

--- queries.sql
+++ queries.sql
@@ -302,3 +302,7 @@ WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name
 ORDER BY species.name DESC 
 LIMIT 1;
+
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+explain analyze SELECT * FROM visits where vet_id = 2;
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
